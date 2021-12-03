e4cd8a39e160e5a49e920ce076aa209a559c329a
lesejong <umlstudy@gmail.com>
2021-12-03 12:16:57 +0900

    ...

--- packages/p31.dao/src/dao/AbsDao.ts
+++ packages/p31.dao/src/dao/AbsDao.ts
@@ -1,4 +1,8 @@
 import { AbsModel } from "@sejong/model";
 export abstract class AbsDao<M extends AbsModel> {
     protected abstract getTableName():string;
+
+    protected showName(model:M):void {
+        console.log(model)
+    }
 }
