8b13e460875b29dd4090175a6e1ad703c631acab
lesejong <umlstudy@gmail.com>
2021-12-10 08:09:46 +0900

    ...

--- packages/p51.graphql/src.generate/generateResolverUsingTbl.ts
+++ packages/p51.graphql/src.generate/generateResolverUsingTbl.ts
@@ -1,28 +1,36 @@
+import { SjChangeCaseUtil, SjTemplateUtil } from '@sejong/common';
+import * as fs from 'fs';
+import { PROJECT_HOME } from '@sejong/dao';
 
 // 실행
-// npx ts-node packages/p51.fe/src.generate/generateSchemaAndResolverUsingTbl.ts
-
-import sqlts from '@rmp135/sql-ts'
-import { SjLogUtil } from "@sejong/common";
-import { knexDefaultSetting } from "@sejong/dao";
-
-// https://github.com/rmp135/sql-ts
+// ts-node packages/p51.graphql/src.generate/generateResolverUsingTbl.ts
 
 async function main() {
-    console.log("----------------");
-    const dbSetting = { ...knexDefaultSetting
-        , "interfaceNameFormat": "${table}Model"
-        , "columnNameCasing": "camel"
-        , "tableNameCasing": "pascal"
-    };
-    console.log("----------------");
-    const definitions = await sqlts.toObject(dbSetting)
-    console.log("----------------");
-    const tsString = sqlts.fromObject(definitions, dbSetting)
+    const srcLoc = `${PROJECT_HOME}\\packages\\p51.graphql`;
+    const targetLoc = srcLoc + "\\src\\graphql\\gen";
 
-    console.log("----------------");
-    SjLogUtil.debug(tsString);
-    console.log("================");
+    const tmplLoc = srcLoc + "\\src.generate\\generateResolverUsingTbl.tmpl";
+    const tmplString = fs.readFileSync(tmplLoc, 'utf8');
+
+    const exportClasses = [] as string[];
+    {
+        const table = "memo";
+        const pascalTableName = SjChangeCaseUtil.convertCase(table, 'pascal');
+        const camelTableName = SjChangeCaseUtil.convertCase(table, 'camel');
+
+        const data = {
+            pascalModelName: pascalTableName
+            , camelModelName: camelTableName
+        };
+
+        const converted = SjTemplateUtil.convert(tmplString, data);
+        fs.writeFileSync(targetLoc + "//Gen" + pascalTableName + "Resolver.ts", converted);
+
+        const exportStr = `export { ${pascalTableName}Biz } from \"./biz/${pascalTableName}Biz\";`;
+        exportClasses.push(exportStr);
+    }
+
+    exportClasses.forEach((s) => console.log(s));
 }
 
 main();
