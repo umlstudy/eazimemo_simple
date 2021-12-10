768bef2a15732b53e3c6fbe34aae20da7b440d04
lesejong <umlstudy@gmail.com>
2021-12-10 12:54:15 +0900

    ...

--- packages/p51.graphql/src/AppSchema.ts
+++ packages/p51.graphql/src/AppSchema.ts
@@ -1,9 +1,8 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { GenMemoResolver } from './graphql/gen/GenMemoResolver';
 import { GenTableTypeDefs } from './graphql/gen/GenTableTypeDefs';
-import { MemoResolver, MemoTypeDefs } from './graphql/resolver/MemoResolver';
 
 export const AppSchema = makeExecutableSchema({
-    typeDefs: [GenTableTypeDefs, MemoTypeDefs],
-    resolvers: [GenMemoResolver, MemoResolver],
+    typeDefs: [GenTableTypeDefs],
+    resolvers: [GenMemoResolver],
 });
