8f77613beeb3406bab1c9dbf920f1ab1a6195e8f
lesejong <umlstudy@gmail.com>
2022-01-06 09:27:50 +0900

    ..

--- packages/p81.frontend/src/index.tsx
+++ packages/p81.frontend/src/index.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-// cd packages/p71.frontend;rm -rf dist;yarn install;tsc;cd ../..
+// cd packages/p81.frontend;rm -rf dist;yarn install;tsc;cd ../..
 
-// cd packages/p71.frontend;rm -rf dist;tsc;cd ../..
+// cd packages/p81.frontend;rm -rf dist;tsc;cd ../..
 
-// cd packages/p71.frontend;yarn start;cd ../..
+// cd packages/p81.frontend;yarn start;cd ../..
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
